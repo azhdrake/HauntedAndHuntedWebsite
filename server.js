308f9a93b270a1c7ecbb8e2b154c3b9d6ea6aad4
B33 <my@email.com>
2019-12-15 19:37:18 -0600

    prepared for heroku deployment

--- server.js
+++ server.js
@@ -6,21 +6,35 @@ var Sequelize = require("sequelize")
 var api_routes = require("./routes/api.js")
 var path = require("path")
 
+db_url = process.env.DATABASE_URL
+
 let sequelize
 
-sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./db.sqlite3"
-})
+if (db_url) {
+  sequelize = new Sequelize(db_url, {
+    dialect: "postgres",
+  })
 
-sequelize.authenticate()
-  .then(() => console.log("connected to sqlite"))
-  .catch(err => console.log("error connecting", err))
+  sequelize.authenticate()
+    .then(() => console.log("Connected to Postgres"))
+    .catch(err => console.log(err))
+} else {
+  sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./db.sqlite3"
+  })
+
+  sequelize.authenticate()
+    .then(() => console.log("connected to sqlite"))
+    .catch(err => console.log("error connecting", err))
+}
 
 let comment = require("./model/Comments.js")(sequelize, Sequelize)
 
-var app = express()
+var app = express
 app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, "webcomic-website", "dist")))
 
 
 app.use("/api", api_routes(comment))
